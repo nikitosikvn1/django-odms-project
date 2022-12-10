6eccf0458e3bd57baecc3421471dc21064e8b5d9
Nikita Petrykin <nvnedu2120@gmail.com>
2022-12-10 02:33:33 +0100

    feat: Layout page login form
    - Login form added

--- publicdb/pdapp/static/pdapp/JS/layout.js
+++ publicdb/pdapp/static/pdapp/JS/layout.js
@@ -1,6 +1,49 @@
 $(document).ready(function() {
+    // Dropdown lists
     $('.sub-button').click(function() {
         $(this).next('.sub-menu').slideToggle();
         $(this).find('.dropdown').toggleClass('rotate');
+    });
+
+    // Popup events
+    const loginPopupBg = document.querySelector('.login-popup-bg');
+    const loginPopupForm = document.querySelector('.login-popup-form');
+    const popupCloseButton = document.querySelector('.close-block img');
+    const popupOpenButtons = document.querySelectorAll('.open-login-form');
+
+    popupOpenButtons.forEach((button) => {
+        button.addEventListener('click', (e) => {
+            e.preventDefault();
+            loginPopupBg.classList.add('active');
+            loginPopupForm.classList.add('active');
+        });
+    });
+
+    popupCloseButton.addEventListener('click', () => {
+        loginPopupBg.classList.remove('active');
+        loginPopupForm.classList.remove('active');
+    });
+
+    document.addEventListener('click', (e) => {
+        if (e.target === loginPopupBg) {
+            loginPopupBg.classList.remove('active');
+            loginPopupForm.classList.remove('active');
+        }
+    });
+
+    // Form submitting
+    const emailField = document.querySelector('.email-field');
+    const passwordField = document.querySelector('.password-field');
+    const submitButton = document.querySelector('#submit');
+
+    loginPopupForm.addEventListener('change', () => {
+        emailLength = emailField.value.length;
+        passwordLength = passwordField.value.length;
+
+        if (emailLength >= 3 && passwordLength >= 8) {
+            submitButton.disabled = false;
+        } else {
+            submitButton.disabled = true;
+        }
     })
 });
