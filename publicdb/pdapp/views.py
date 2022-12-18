3d9a7cc1205aa4102999a850b32875eeda1a922a
Nikita Petrykin <nvnedu2120@gmail.com>
2022-12-18 04:01:29 +0100

    feat: Login & registration views
    - Added logic for login and registration on the site

--- publicdb/pdapp/views.py
+++ publicdb/pdapp/views.py
@@ -8,15 +8,22 @@ from .forms import RegistrationForm
 # Create your views here.
 
 def index(request):
-    logged = False
-
-    return render(request, "pdapp/index.html", {
-        "logged": logged
-    })
+    return render(request, "pdapp/index.html")
 
 
 def registration_view(request):
     form = RegistrationForm()
+
+    if request.method == "POST":
+        form = RegistrationForm(request.POST)
+        if form.is_valid():
+            user = form.save(commit=False)
+            user.save()
+
+            user = authenticate(request, username=user.username, password=request.POST["password1"])
+            if user is not None:
+                login(request, user)
+                return HttpResponseRedirect(reverse('index'))
 
     return render(request, "pdapp/registration.html", {
         "form": form,
@@ -27,13 +34,25 @@ def login_view(request):
     if request.method == "POST":
         username = request.POST["username"]
         password = request.POST["password"]
-        print(f"{username=}, {password=}")
+        user = authenticate(request, username=username, password=password)
+
+        if user is not None:
+            login(request, user)
+            return HttpResponseRedirect(reverse('index'))
+        else:
+            return render(request, "pdapp/login.html", {
+                "message": "Invalid credentials."
+            })
 
     return render(request, "pdapp/login.html")
 
 
 def logout_view(request):
-    pass
+    logout(request)
+
+    return render(request, "pdapp/login.html", {
+        "message": "Logged out."
+    })
 
 def profile(request):
     return HttpResponse("<h2>Your profile will be here!</h2>")
