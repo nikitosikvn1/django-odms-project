55541c0c173b862460b00c1008b500ce545e9fad
Nikita Petrykin <nvnedu2120@gmail.com>
2023-06-07 19:47:33 +0200

    fix: Fixed has_group template tag

--- publicdb/pdapp/templatetags/has_group.py
+++ publicdb/pdapp/templatetags/has_group.py
@@ -4,6 +4,10 @@ from django.contrib.auth.models import Group
 register = template.Library() 
 
 @register.filter() 
-def has_group(user, group_name): 
-    group = Group.objects.get(name=group_name) 
+def has_group(user, group_name):
+    try:
+        group = Group.objects.get(name=group_name)
+    except Group.DoesNotExist:
+        return False
+    
     return group in user.groups.all() 
